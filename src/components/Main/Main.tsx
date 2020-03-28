cbd68ae4f14395db39e0736190a38ffa1bd6d65d
Craig Thomas <craigt672@gmail.com>
2020-03-28 16:03:43 -0400

    [CT] import various components in home page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,10 +2,16 @@ import React, { useContext } from 'react';
 
 import Contact from '../../components/Contact/Contact';
 import LatestNews from '../../components/LatestNews/LatestNews';
+import TopReads from '../../components/TopReads/TopReads';
+import HealthNews from '../../components/HealthNews/HealthNews';
+import FeaturedAuthors from '../../components/FeaturedAuthors/FeaturedAuthors';
 
 import NavContext, { NAV_CLOSE } from '../../contexts/nav';
 
 import styles from './Main.module.scss';
+import { Billboard } from '../Ads/Ads';
+import Research from '../Research/Research';
+import Tools from '../Tools/Tools';
 
 export default function Main() {
   const [ navState, dispatch ]: any[] =  useContext(NavContext);
@@ -17,6 +23,11 @@ export default function Main() {
   return (
     <div className={styles.container} onClick={closeNav}>
       <LatestNews />
+      <TopReads />
+      <HealthNews />
+      <Research />
+      <Tools />
+      <FeaturedAuthors />
     </div>
   )
 }
