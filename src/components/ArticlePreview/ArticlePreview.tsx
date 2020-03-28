24a5908a63a02a5fbec5e874cf47e4d3b7731788
Craig Thomas <craigt672@gmail.com>
2020-03-28 11:23:44 -0400

    [CT] Remove 'Read more' text from Post-Preview

--- src/components/ArticlePreview/ArticlePreview.tsx
+++ src/components/ArticlePreview/ArticlePreview.tsx
@@ -1,55 +1,62 @@
 import React from 'react';
 
-import { slugify }from '../../util/createSlug';
+import { slugify } from '../../util/createSlug';
 import styles from './ArticlePreview.module.scss';
 import { Link } from 'gatsby';
 
-export function FirstArticlePreview({ image, title, description  }: any) {
+export function FirstArticlePreview({ image, title, description }: any) {
   const slug = slugify(title);
   const link = `/news/${slug}`
-  console.log('title: ', title);
-  console.log('slug: ', slug);
+
+  const descriptionText = description.replace('Read more...', '');
+
+  console.log('text: ', descriptionText);
 
   return (
     <div className={styles.prevContainer}>
       <Link
         className={styles.img}
-        to={link} 
-        style={{backgroundImage: `url(${image})`}}>   
+        to={link}
+        style={{ backgroundImage: `url(${image})` }}>
       </Link>
       <div className={styles.body}>
-        <h5 className={styles.title}>
-          {title}
-        </h5>
-        <p className={styles.description}>
-          {description}
-        </p>
+        <Link className={styles.link} to={link}>
+          <h5 className={styles.title}>
+            {title}
+          </h5>
+          <p className={styles.description}>
+            {descriptionText}
+          </p>
+        </Link>
       </div>
     </div>
   )
 }
 
-function ArticlePreview({ image, title, description  }: any) {
+function ArticlePreview({ image, title, description }: any) {
   const slug = slugify(title);
   const link = `/news/${slug}`
 
-  console.log('title: ', title);
-  console.log('slug: ', slug);
+  const descriptionText = description.replace('Read more...', '');
+
+  console.log('text: ', descriptionText);
 
   return (
     <div className={styles.container}>
       <Link
         className={styles.img}
-        to={link} 
-        style={{backgroundImage: `url(${image})`}}>   
+        to={link}
+        style={{ backgroundImage: `url(${image})` }}>
       </Link>
       <div className={styles.body}>
-        <h5 className={styles.title}>
-          {title}
-        </h5>
-        <p className={styles.description}>
-          {description}
-        </p>
+        <Link className={styles.link} to={link}>
+          <h5 className={styles.title}>
+            {title}
+          </h5>
+          <p className={styles.description}>
+            {descriptionText}
+          </p>
+        </Link>
       </div>
     </div>
   )
