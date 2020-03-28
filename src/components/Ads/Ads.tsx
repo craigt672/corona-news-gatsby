d2812885c9e41727e211e0d282ad6469e1c01da4
Craig Thomas <craigt672@gmail.com>
2020-03-28 15:56:13 -0400

    [CT] Ad Components

---
+++ src/components/Ads/Ads.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Ads.module.scss';
+
+export function Stm() {
+  return <div className={styles.stm}></div>
+}
+
+export function Billboard() {
+  return <div className={styles.billboard}></div>
+}
+
+export function Rail() {
+  return <div className={styles.rail}></div>
+}
+
+export function RailLong() {
+  return <div className={styles.railLong}></div>
+}
+
+export function RailSticky() {
+  return <div className={styles.railSticky}></div>
+}
